771853a5d9e34a728f2b7b373db409463a39bdce
Adam Ayers <adam@number5.com>
2026-04-28 11:53:37 -0700

    Update doomscrollr formula to 0.2.0

--- Formula/doomscrollr.rb
+++ Formula/doomscrollr.rb
@@ -1,8 +1,8 @@
 class Doomscrollr < Formula
   desc "DOOMSCROLLR developer front door for owned-audience websites"
   homepage "https://doomscrollr.com"
-  url "https://registry.npmjs.org/doomscrollr/-/doomscrollr-0.1.1.tgz"
-  sha256 "226101e467c86b73e859343c55c0dde0b38f3d65548dfd31ad37b6c8c2ada73f"
+  url "https://registry.npmjs.org/doomscrollr/-/doomscrollr-0.2.0.tgz"
+  sha256 "3e03231df0517d1122dd69b0841517306fa9fcb3b2e74f990b20bfe4856c3e5c"
   license "MIT"
 
   depends_on "node"
